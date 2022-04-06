e6c7e55e68992715ce16e0f49b815d08130ca2ca
PedroLopes7K <pedrohenri3601@gmail.com>
2022-04-05 21:30:58 -0300

    Update 05/04

--- ByteBank.js
+++ ByteBank.js
@@ -1,38 +1,22 @@
-class Cliente {
-  nome
-  cpf
-}
-class ContaCorrente {
-  agencia
-  saldo
-  sacar(valor) {
-    if (this.saldo >= valor) {
-      this.saldo -= valor
-    }
-  }
-}
-const contaCorrenteRicardo = new ContaCorrente()
-
-contaCorrenteRicardo.saldo = 0
-contaCorrenteRicardo.agencia = 1001
-// console.log(contaCorrenteRicardo.saldo)
-contaCorrenteRicardo.saldo = 100
-console.log(contaCorrenteRicardo.saldo)
-contaCorrenteRicardo.sacar(70)
-
-console.log(contaCorrenteRicardo.saldo)
+import { ContaCorrente } from './ContaCorrente.js'
+import { Cliente } from './Cliente.js'
 
 const cliente1 = new Cliente()
 cliente1.nome = 'Ricardo'
 cliente1.cpf = 11122233309
-cliente1.agencia = 1001
-cliente1.saldo = 0
 
 const cliente2 = new Cliente()
 cliente2.nome = 'Alice'
 cliente2.cpf = 88822233309
-cliente2.agencia = 1001
-cliente2.saldo = 0
 
-// console.log(cliente1)
-// console.log(cliente2)
+const contaCorrenteRicardo = new ContaCorrente()
+contaCorrenteRicardo.agencia = 1001
+
+contaCorrenteRicardo.depositar(-100)
+contaCorrenteRicardo.depositar(100)
+contaCorrenteRicardo.depositar(100)
+
+const valorSacado = contaCorrenteRicardo.sacar(50)
+console.log(valorSacado)
+
+console.log(contaCorrenteRicardo)
